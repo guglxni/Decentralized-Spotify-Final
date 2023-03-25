74a9135036e3e42315f09df2aebfef0bd1eb38c9
Aaryan Guglani <84870556+guglxni@users.noreply.github.com>
2023-03-25 08:25:24 +0530

    Add files via upload

---
+++ Decentralized-Spotify-Final/Decentralized-Spotify-main/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+
+// Setup
+import { Network, Alchemy } from 'alchemy-sdk';
+
+const settings = {
+    apiKey: "oE6vx59kylTMksVqySlzAI26oSw1s3Lr",
+    network: Network.MATIC_MUMBAI,
+};
+
+const alchemy = new Alchemy(settings);
+
+// Get the latest block
+const latestBlock = alchemy.core.getBlockNumber();
+
+// Get all outbound transfers for a provided address
+alchemy.core
+    .getTokenBalances('0x994b342dd87fc825f66e51ffa3ef71ad818b6893')
+    .then(console.log);
+
+// Get all the NFTs owned by an address
+const nfts = alchemy.nft.getNftsForOwner("0xshah.eth");
+
+// Listen to all new pending transactions
+alchemy.ws.on(
+    { method: "alchemy_pendingTransactions",
+    fromAddress: "0xshah.eth" },
+    (res) => console.log(res)
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="xxxx" serverUrl="xxxx">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
